f1d10f410e90b374bbc35f20c115a0a88ec9a403
Ehsan Tabatabaei <ehsantaba2010@gmail.com>
2015-09-18 09:44:56 -0700

    #2 Created worksheets json file to house all worksheet data

--- server.js
+++ server.js
@@ -2,14 +2,18 @@ var express = require('express');
 var app = express();
 var user = require('./login.js');
 var path = require('path');
-var emotions = require('./emotions.js');
-var questions = require('./questions.js');
 app.use(express.static(__dirname + '/public'));
-app.use('/questions', questions);
+app.use('/login', user);
+app.get('/emotionList.json', function(req, res){
+   console.log(JSON.stringify(req.body));
+   res.sendFile(path.join(__dirname + '/emotionList.json'));
+});
+app.get('/worksheets.json', function(req, res){
+   console.log(JSON.stringify(req.body));
+   res.sendFile(path.join(__dirname + '/worksheets.json'));
+});
 app.get('/', function(req, res){
    res.sendFile(path.join(__dirname + '/index.html'));
 });
-//app.use('/login', user);
-app.use('/emotions', emotions);
 app.listen(1337);
 console.log('Server running. Port 1337');
