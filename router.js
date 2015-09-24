d4783b55db5813d44fbe158876056448663b6908
Ehsan Tabatabaei <ehsantaba2010@gmail.com>
2015-09-24 16:38:27 -0700

    Changed the indentation from 4 spaces to 2 to comply with styleguide

--- router.js
+++ router.js
@@ -5,46 +5,46 @@ var bodyParser = require('body-parser');
 var jsonParser = bodyParser.json();
 var fs = require('fs');
 router.get('/answers/:id', function(req, res) {
-    var id = req.params.id;
-    console.log('Router.get received your request');
-    fs.readdir('./' + id, function (err, files) {
-        if (err) {
-            console.log("Error reading files: ", err);
-        } else {
-            var answers = [];
-            files.forEach(function(file){
-               console.log(file);
-            });
-            for (var counter = 0; counter < files.length; counter++){
-                var answer = fs.readFileSync(__dirname + '/' + id + '/' + files[counter],  "utf-8");
-                answers.push(JSON.parse(answer))
-            }
-            res.send(answers);
-        }
-    });
+  var id = req.params.id;
+  console.log('Router.get received your request');
+  fs.readdir('./' + id, function (err, files) {
+    if (err) {
+      console.log("Error reading files: ", err);
+    } else {
+      var answers = [];
+      files.forEach(function(file){
+        console.log(file);
+      });
+      for (var counter = 0; counter < files.length; counter++){
+        var answer = fs.readFileSync(__dirname + '/' + id + '/' + files[counter],  "utf-8");
+        answers.push(JSON.parse(answer))
+      }
+      res.send(answers);
+    }
+  });
 });
 router.post('/answers/:id', jsonParser, function (req, res) {
-    console.log('hello world');
-    var id = req.params.id;
-    fs.stat('./' + id, function (err, stats) {
-        if (err) {
-            fs.mkdirSync('./' + id);
-        }
-        console.log(stats);
-    });
-    var random = Math.floor((Math.random() * 1000) + 1);
-    var dateNow = Date.now();
-    //var dateScan = new Date().toDateString();
-    var PATH_NAME = __dirname;
-    fs.writeFile(PATH_NAME + '/' + id + '/' + dateNow + ':' + random + '.json', JSON.stringify(req.body), function (error) {
-        if (error) {
-            fs.writeFileSync(PATH_NAME + '/' + id + '/' + dateNow + ':' + random + '.json', JSON.stringify(req.body));
-        }
-    });
-    res.send('File received ' + req.body);
+  console.log('hello world');
+  var id = req.params.id;
+  fs.stat('./' + id, function (err, stats) {
+    if (err) {
+      fs.mkdirSync('./' + id);
+    }
+    console.log(stats);
+  });
+  var random = Math.floor((Math.random() * 1000) + 1);
+  var dateNow = Date.now();
+  //var dateScan = new Date().toDateString();
+  var PATH_NAME = __dirname;
+  fs.writeFile(PATH_NAME + '/' + id + '/' + dateNow + ':' + random + '.json', JSON.stringify(req.body), function (error) {
+    if (error) {
+      fs.writeFileSync(PATH_NAME + '/' + id + '/' + dateNow + ':' + random + '.json', JSON.stringify(req.body));
+    }
+  });
+  res.send('File received ' + req.body);
 });
 router.get('/', function(req, res){
-    res.send("GOT IT");
+  res.send("GOT IT");
 });
 module.exports = router;
 //// piping the data
--- server.js
+++ server.js
@@ -4,19 +4,19 @@ var path = require('path');
 var myRoute = require('./router');
 app.use(express.static(__dirname + '/public'));
 app.use('/secureFormData',  myRoute, function(req, res){
-   console.log('OOPS> STOPS HERE');
-   res.send(req.url);
+  console.log('OOPS> STOPS HERE');
+  res.send(req.url);
 });
 app.get('/emotions.json', function(req, res){
-   console.log(JSON.stringify(req.body));
-   res.sendFile(path.join(__dirname + '/emotions.json'));
+  console.log(JSON.stringify(req.body));
+  res.sendFile(path.join(__dirname + '/emotions.json'));
 });
 app.get('/worksheets.json', function(req, res){
-   console.log(JSON.stringify(req.body));
-   res.sendFile(path.join(__dirname + '/worksheets.json'));
+  console.log(JSON.stringify(req.body));
+  res.sendFile(path.join(__dirname + '/worksheets.json'));
 });
 app.get('/', function(req, res){
-   res.sendFile(path.join(__dirname + '/index.html'));
+  res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.listen(1337);
 console.log('Server running. Port 1337');
