24e03648f2fd2502d9db20d043f3888c626fa897
Ehsan Tabatabaei <ehsantaba2010@gmail.com>
2015-09-28 10:24:02 -0700

    refactoring code

--- public/history.js
+++ public/history.js
@@ -1,74 +1,71 @@
-appendHistory();
-function appendHistory(){
-    function appendLog(sections){
-        console.log(sections.logEntry);
-        var display = document.createElement('tbody');
-        document.querySelector('.table.table-bordered').appendChild(display);
-        var row = document.createElement('tr');
-        display.appendChild(row);
-        var dates = document.createElement('td');
-        row.appendChild(dates);
-        var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
-        var eventDate = new Date(sections.eventDate);
-        var localeTime = eventDate.toLocaleString('en-US', options);
-        dates.innerHTML = '<p>' + localeTime + '</p>';
-        var descriptions = document.createElement('td');
-        descriptions.innerHTML = '<p>' + sections.eventDescription + '</p>';
-        row.appendChild(descriptions);
-        var ratedEmotions = sections.emotionsAndRange;
-        var emotionsList = document.createElement('td');
-        emotionsList.className = 'catalog';
-        row.appendChild(emotionsList);
-        var thoughtsList = document.createElement('td');
-        thoughtsList.className = 'thoughts-list';
-        row.appendChild(thoughtsList);
-        for (var key in ratedEmotions) {
-            if (ratedEmotions.hasOwnProperty(key)) {
-                var passage = document.createElement('p');
-                passage.setAttribute('data-emotion', key);
-                passage.setAttribute('data-range', ratedEmotions[key]);
-                emotionsList.appendChild(passage);
-                passage.textContent = key + ' ' + '(' + ratedEmotions[key] + '%' + ')';
-            }
-        }
-        var negativeThoughts = sections.automaticNegativeThoughts;
-        for (var thoughts in negativeThoughts) {
-            if (negativeThoughts.hasOwnProperty(thoughts)) {
-                var newThought = document.createElement('p');
-                newThought.className = 'thoughts';
-                newThought.setAttribute('data-toggle', 'modal');
-                newThought.setAttribute('data-target', '#affirm');
-                thoughtsList.appendChild(newThought);
-                newThought.textContent = negativeThoughts[thoughts];
-            }
-        }
-        var ratedList = document.createElement('td');
-        row.appendChild(ratedList);
-        for (var property in ratedEmotions) {
-            if (ratedEmotions.hasOwnProperty(property)) {
-                var passageThree = document.createElement('p');
-                ratedList.appendChild(passageThree);
-              passageThree.textContent = property + ' ( __%)';
-            }
+function appendLog(sections){
+    console.log(sections.logEntry);
+    var display = document.createElement('tbody');
+    document.querySelector('.table.table-bordered').appendChild(display);
+    var row = document.createElement('tr');
+    display.appendChild(row);
+    var dates = document.createElement('td');
+    row.appendChild(dates);
+    var options = { weekday: 'short', year: 'numeric', month: 'short', day: 'numeric', hour: 'numeric', minute: 'numeric' };
+    var eventDate = new Date(sections.eventDate);
+    var localeTime = eventDate.toLocaleString('en-US', options);
+    dates.innerHTML = '<p>' + localeTime + '</p>';
+    var descriptions = document.createElement('td');
+    descriptions.innerHTML = '<p>' + sections.eventDescription + '</p>';
+    row.appendChild(descriptions);
+    var ratedEmotions = sections.emotionsAndRange;
+    var emotionsList = document.createElement('td');
+    emotionsList.className = 'catalog';
+    row.appendChild(emotionsList);
+    var thoughtsList = document.createElement('td');
+    thoughtsList.className = 'thoughts-list';
+    row.appendChild(thoughtsList);
+    for (var key in ratedEmotions) {
+        if (ratedEmotions.hasOwnProperty(key)) {
+            var passage = document.createElement('p');
+            passage.setAttribute('data-emotion', key);
+            passage.setAttribute('data-range', ratedEmotions[key]);
+            emotionsList.appendChild(passage);
+            passage.textContent = key + ' ' + '(' + ratedEmotions[key] + '%' + ')';
         }
     }
-    var xhr = new XMLHttpRequest();
-    xhr.addEventListener('load', function(e) {
-        if((xhr.status === 200) || (xhr.status === 304)) {
-            var response = xhr.responseText;
-            console.log(response);
-            var stressLog = JSON.parse(response);
-            stressLog.forEach(function(section){
-                appendLog(section);
-            });
+    var negativeThoughts = sections.automaticNegativeThoughts;
+    for (var thoughts in negativeThoughts) {
+        if (negativeThoughts.hasOwnProperty(thoughts)) {
+            var newThought = document.createElement('p');
+            newThought.className = 'thoughts';
+            newThought.setAttribute('data-toggle', 'modal');
+            newThought.setAttribute('data-target', '#affirm');
+            thoughtsList.appendChild(newThought);
+            newThought.textContent = negativeThoughts[thoughts];
         }
-        else {
-            alert(e.target.responseText);
+    }
+    var ratedList = document.createElement('td');
+    row.appendChild(ratedList);
+    for (var property in ratedEmotions) {
+        if (ratedEmotions.hasOwnProperty(property)) {
+            var passageThree = document.createElement('p');
+            ratedList.appendChild(passageThree);
+          passageThree.textContent = property + ' ( __%)';
         }
-    });
-    xhr.addEventListener("error", function() {
-        alert('Something went wrong. Something did not load properly. Check router and server configurations');
-    });
-    xhr.open('GET', './secureFormData/answers/5000/stress-logs', true);
-    xhr.send();
+    }
 }
+var xhr = new XMLHttpRequest();
+xhr.addEventListener('load', function(e) {
+    if((xhr.status === 200) || (xhr.status === 304)) {
+        var response = xhr.responseText;
+        console.log(response);
+        var stressLog = JSON.parse(response);
+        stressLog.forEach(function(section){
+            appendLog(section);
+        });
+    }
+    else {
+        alert(e.target.responseText);
+    }
+});
+xhr.addEventListener("error", function() {
+    alert('Something went wrong. Something did not load properly. Check router and server configurations');
+});
+xhr.open('GET', './secureFormData/answers/5000/stress-logs', true);
+xhr.send();
--- router.js
+++ router.js
@@ -18,11 +18,7 @@ router.get('/answers/:id/:submission', function(req, res) {
         console.log('this is the file: ' + file);
       });
       for (var counter = 0; counter < files.length; counter++){
-        // YOU CAN ALSO DO THE WRITE TO FILE[INDEX] THE NAME OF THE FILE BY CALLING A METHOD
-        // THAT LETS YOU ADD STRING TO A FILE
-        // TRY FS APPENDFILE
         var answer = fs.readFileSync(__dirname + '/' + id + '/' + submission + '/' + files[counter],  "utf-8");
-        console.log('answer[eventDate]' + answer['eventDate']);
         console.log(answer);
         answers.push(JSON.parse(answer));
       }
@@ -58,39 +54,17 @@ router.post('/answers/:id/:submission', jsonParser, function (req, res) {
   watcher
       .on('change', function(path) {
         console.log('File', path, 'has been added');
-        // *NEW*: GET THE PROPERTY "stress-logEntry" AND IT'S CORRESPONDING FILE NAME
-        // READFILe here
-        // DO SOMETHING ASSOCIATE THE STRESS OG FILE NAME WITH THE CHALLNGE EXERCISE ITS ASSOCUATED WITH
         var answer = fs.readFileSync(path,  "utf-8");
         console.log(answer);
         var configJSON = JSON.parse(answer);
         configJSON["logEntry"] = path;
         var mew = JSON.stringify(configJSON);
         fs.writeFileSync(path, mew);
-        // GET THE PATH OF THE CHANGED FILE. IN THIS ON FUNCTION YOU CAN CREATEWRITESTREAM AND PIPE IT TO
-        // COMPLETED SECTION AND ADD THE STRING "COMPLETED" TO THE STRESS LOG THEY USED TO COMPLETE IT
-        // REMEBER THAT WHEN YOU FETCH THE STRESS LOG HISTORY IN HISTORY JS
-        // YOU CAN ALSO GET THE FILE NAME ASSOCIATED WITH EACH INDIVIDUAL STRESS LOG
-        // WHEN THE USER COMPLETES A CHALLENGE GET THE FILE NAME AND SEND IT TO THE BACK
-        // STREAMWRITE INTO THAT FILE IN JSON FORMAT AND WRITE COMPLETED
-
-
-        //  IDEA: YOU CAN ADD THE STRESS LOG FILE NAME INSIDE THE FILE WHEN YOU SEND IT BACK FROM A REQUEST
-        // THEN WHEN IT IS ADDED IN HISTORY YOU CAN EXTRACT IN IN THE EXERCISE FILE
-        // SINCE YOU ALREADY MAKE A REQUEST BUT YOU DONT ACTUALLY EXTARCT ANY OF THE JSON THINGS
-        // THEN SINCE IT'S BY ORDER YOU KNOW AUTOMATICALLY WHICH ONE CORRESPONDS TO WHICH
-
-        // SEE SAVED PICTURE IN PHONE. "PIPING STREAM IS TAKING THE OUTPUT OF ONE STREAM AND FEEDING IT
-        // INTO THE INPUT OF ANOTHER"
-        //var paths = [];
         fs.stat(path, function (err, stat) {
-          // Replace error checking with something appropriate for your app.
           if (err) throw err;
-          console.log('hi hi, this is the "stat" :' + stat);
-          //setTimeout(checkEnd, end_timeout, path, stat);
+          console.log('This is the "stat" from watcher:' + stat);
         });
         watcher.unwatch('./' + id + '/' + 'stress-logs');
-        //console.log(paths)
       });
   var random = Math.floor((Math.random() * 1000) + 1);
   var dateNow = Date.now();
@@ -104,19 +78,4 @@ router.post('/answers/:id/:submission', jsonParser, function (req, res) {
 router.get('/', function(req, res){
   res.send("GOT IT");
 });
-//chokidar.watch('.', {ignored: /[\/\\]\./}).on('all', function(event, path) {
-//  console.log(event, path);
-//});
-
 module.exports = router;
-//// piping the data
-//var readable = getReadableStreamSomehow();
-//readable.on('data', function(chunk) {
-//    console.log('got %d bytes of data', chunk.length);
-//});
-//readable.on('end', function() {
-//    console.log('there will be no more data.');
-//});
-// watch stream and see any changes
-// get the file changes and send the captured file path to redirect
-// check out https://www.npmjs.com/package/send
