fca314bb69c441516e4032485ff1ff13f4b62634
Ehsan Tabatabaei <ehsantaba2010@gmail.com>
2015-09-30 19:10:29 -0700

    Test server with Mocha

--- test.js
+++ test.js
@@ -1,120 +1,21 @@
 var express = require('express');
 var should = require('should');
 var request = require('supertest');
+var answers = require('./answers');
 
-describe('Express Route Test', function () {
+describe('GET stress-logs', function(){
   var app = express();
+
+  app.use('/secureFormData', answers);
+
   var agent = request.agent(app);
 
-  app.get('/', function (req, res) {
-    res.send('hello world');
-  });
-
-  it('status code 200',  function(done){
+  it('respond with json', function(done){
     agent
-        .get('/')
+        .get('/secureFormData/answers/5000/stress-logs')
+        .set('Accept', 'application/json')
+        .expect('Content-Type', /json/)
         .expect(200)
         .end(done)
-  });
-
-  describe('get json files',  function() {
-    app.use(express.static(__dirname + '/json'));
-
-    it('GET /emotions.json', function (done) {
-      agent
-          .get('/emotions.json')
-          .set('Accept', 'application/json')
-          .expect(function (res) {
-            res.bod = [
-              "Aggravation",
-              "Agitation",
-              "Agony",
-              "Alarm",
-              "Alienation",
-              "Anger",
-              "Anguish",
-              "Annoyance",
-              "Anxiety",
-              "Apprehension (fear)",
-              "Bitter",
-              "Contempt",
-              "Crosspatch",
-              "Defeatism",
-              "Dejection",
-              "Depression",
-              "Despair",
-              "Disappointment",
-              "Disgust",
-              "Dislike",
-              "Dismay",
-              "Displeasure",
-              "Distress",
-              "Dread",
-              "Embarrassment",
-              "Envy",
-              "Exasperation",
-              "Fear",
-              "Ferocity",
-              "Fright",
-              "Frustration",
-              "Fury",
-              "Gloom",
-              "Glumness",
-              "Grief",
-              "Grouchy",
-              "Grumpy",
-              "Guilt",
-              "Hatred",
-              "Homesickness",
-              "Horror",
-              "Hostility",
-              "Humiliation",
-              "Hurt",
-              "Hysteria",
-              "Insecurity",
-              "Insult",
-              "Irritability",
-              "Isolation",
-              "Jealousy",
-              "Loathing",
-              "Loneliness",
-              "Melancholy",
-              "Misery",
-              "Mono no aware",
-              "Mortification",
-              "Neglect",
-              "Nervousness",
-              "Outrage",
-              "Panic",
-              "Pity",
-              "Rage",
-              "Regret",
-              "Rejection",
-              "Remorse",
-              "Resentment",
-              "Revulsion",
-              "Sadness",
-              "Scorn",
-              "Shame",
-              "Shock",
-              "Sorrow",
-              "Spite",
-              "Suffering",
-              "Suspense",
-              "Sympathy",
-              "Terror",
-              "Torment",
-              "Torment",
-              "Uneasiness",
-              "Unhappy",
-              "Vengefulness",
-              "Woe",
-              "Worry",
-              "Wrath"
-            ]
-          })
-          .expect(200)
-          .end(done)
-    })
   });
 });
